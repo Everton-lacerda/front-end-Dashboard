8584c42572f3b566ed66aae53082adfdf294e90c
Everton-Lacerda <everton3b@hotmail.com>
2020-01-20 12:11:17 -0300

    page add produto

--- js/script.js
+++ js/script.js
@@ -26,12 +26,37 @@ $(function ($) {
   });
   $("#choose-black").click(function() {
     $('.sidebar').css({'background-color': '#1a2035'});
-  });
-
-
-   
+  });  
    
 });
+
+
+$(document).on("click", ".browse", function() {
+  var file = $(this).parents().find(".file");
+  file.trigger("click");
+});
+$('input[type="file"]').change(function(e) {
+  var fileName = e.target.files[0].name;
+  $("#file").val(fileName);
+
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    // get loaded data and render thumbnail.
+    document.getElementById("preview").src = e.target.result;
+  };
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+});
+
+$(".btn-add").click(function(){
+  Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Cadastro feito com sucesso!',
+      showConfirmButton: false,
+      timer: 2500
+  })
+})
 
 // $(function () {
     // $(".wrapper .sidebar .sidebar-list > li:first-child").addClass('active');
